ccb4ebabdcb15ea4f20a004dfe7447d843d21a5b
David <david.nunez@oktana.io>
2021-01-31 16:19:22 -0300

    Basic data for every stock

---
+++ components/ShowMoreData.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const ShowMoreData = ({stockData}) => {
+    console.log(stockData);
+    
+    const dividedData = Object.entries( stockData.timeSeries['Time Series (Daily)'] );
+    const dates = dividedData.map(elmt => elmt[0]).reverse();
+    const dataForDates = dividedData.map(elmt => elmt[1]['2. high']).reverse();
+    const startedDate = new Date(dates[0]);
+    const initialValue = parseInt(dataForDates[0]);
+    const finalValue = parseInt(dataForDates[ dataForDates.length - 1]);
+    const growth = (finalValue - initialValue) / initialValue * 100;
+    const timePass = Math.abs( new Date(dates[dates.length -1]) - new Date(dates[0]) ) / 1000 / 60 / 60 / 24 ;
+    return(
+        <div className="row mt-3">
+            <div className="col-12">
+                <div className="shadow p-3 mb-5 bg-white rounded">
+                    <h5 className="text-center">Relevant data for {stockData.companyData.Name}</h5>
+                    <div className="mt-3">
+                        <p>From {initialValue}$ to {finalValue}$</p>
+                        <p>Growth of {growth.toFixed(1)} % in {dates.length} days since {startedDate.toLocaleDateString()}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default ShowMoreData;
