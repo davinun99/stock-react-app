d04a44c3843ecd188e3d913db19b29d52ff9417d
David Nuñez <nundavid99@gmail.com>
2021-04-26 10:56:40 -0400

    Update MainPortfolio.jsx
    ADDED IWM

--- components/portfolio/MainPortfolio.jsx
+++ components/portfolio/MainPortfolio.jsx
@@ -3,7 +3,8 @@ import PortfolioGraph from './PortfolioGraph';
 const MainPortfolio = () => {
     const assets = [
         {symbol: 'IVV', dateBought: new Date('2021-01-25 09:00'), amount: 0.051558, price: 20 },
-        {symbol: 'AAPL', dateBought: new Date('2021-02-01 09:00'), amount: 0.15016, price: 20 }
+        {symbol: 'AAPL', dateBought: new Date('2021-02-01 09:00'), amount: 0.15016, price: 20 },
+        {symbol: 'IWM', dateBought: new Date('2021-04-26 09:00'), amount: 0.0871, price: 20 }
     ];
     return(
         <>
